db5f4edf2e2c2ce080b6970094362f079759760f
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-01 18:24:59 +0530

    Time: 7 ms (16.64%), Space: 10.2 MB (7.73%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -14,31 +14,26 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p == NULL || q == NULL)
             return p == q;
-        if(p->val != q->val)
-            return 0;
-        
-        queue<TreeNode*> q1, q2;
-        q1.push(p), q2.push(q);
-        while(!q1.empty()) {
-            int s1 = q1.size(), s2 = q2.size();
-            if(s1 != s2)
-                return 0;
-            for(int i = 0; i < s1; i++) {
-                TreeNode *n1 = q1.front(), *n2 = q2.front();
-                q1.pop(), q2.pop();
+        queue<TreeNode*> qu;
+        qu.push(p), qu.push(q);
+        while(!qu.empty()) {
+            int size = qu.size();
+            for(int i = 0; i < size/2; i++) {
+                TreeNode* n1 = qu.front(); qu.pop();
+                TreeNode* n2 = qu.front(); qu.pop();
                 if(n1 == NULL || n2 == NULL) {
-                    if(n1 != n2)
+                    if(n1 != n2) 
                         return 0;
-                    else
+                    else 
                         continue;
                 }
-                if(n1->val != n2->val)
+                else if(n1->val != n2->val)
                     return 0;
-                q1.push(n1->left);
-                q2.push(n2->left);
-                q1.push(n1->right);
-                q2.push(n2->right);
-            }
+                qu.push(n1->left);
+                qu.push(n2->left);
+                qu.push(n1->right);
+                qu.push(n2->right);
+            }            
         }
         return 1;
     }
