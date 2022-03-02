25583d352745d4500972d7a63babcc4037f35326
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-02 07:42:43 +0530

    Time: 29 ms (14.23%), Space: 8.6 MB (80.27%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res = nums2;
+        stack<int> st;
+        for(int i = nums2.size()-1; i >= 0; i--) {
+            
+            while(!st.empty() && st.top() < nums2[i])
+                st.pop();
+            
+            if(st.empty())
+                res[i] = -1;
+            else 
+                res[i] = st.top();
+            
+            st.push(nums2[i]);
+        }
+        //for(int x : res) cout<<x<<" ";
         for(int i = 0; i < nums1.size(); i++) {
-            bool flag = false, sign = false;
             for(int j = 0; j < nums2.size(); j++) {
-                
-                if(flag && nums2[j] > nums1[i]) {
-                    sign = true;
-                    nums1[i] = nums2[j];
+                if(nums1[i] == nums2[j]) {
+                    nums1[i] = res[j];
                     break;
                 }
-                else if(nums1[i] == nums2[j]) {
-                    flag = true;
-                }
             }
-            if(sign == false)
-                nums1[i] = -1;
         }
         return nums1;
     }
