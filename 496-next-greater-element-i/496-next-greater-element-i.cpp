9f966abdcc7c6c3b043ebce6e0067062bb8f14aa
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-02 07:44:45 +0530

    Time: 3 ms (97.06%), Space: 9 MB (33.42%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> res = nums2;
+        unordered_map<int, int> map;
         stack<int> st;
         for(int i = nums2.size()-1; i >= 0; i--) {
             
@@ -9,20 +9,15 @@ public:
                 st.pop();
             
             if(st.empty())
-                res[i] = -1;
+                map[nums2[i]] = -1;
             else 
-                res[i] = st.top();
+                map[nums2[i]] = st.top();
             
             st.push(nums2[i]);
         }
         //for(int x : res) cout<<x<<" ";
         for(int i = 0; i < nums1.size(); i++) {
-            for(int j = 0; j < nums2.size(); j++) {
-                if(nums1[i] == nums2[j]) {
-                    nums1[i] = res[j];
-                    break;
-                }
-            }
+            nums1[i] = map[nums1[i]];
         }
         return nums1;
     }
