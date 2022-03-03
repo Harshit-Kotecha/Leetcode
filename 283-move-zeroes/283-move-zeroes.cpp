cf7b5caa1e1319989128d2965baa1d7848de30dc
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-03 08:11:37 +0530

    Time: 31 ms (61.36%), Space: 19.3 MB (13.91%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int k = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != 0)
+                nums[k++] = nums[i];
+        }
+        while(k < nums.size())
+            nums[k++] = 0;
+    }
+};
