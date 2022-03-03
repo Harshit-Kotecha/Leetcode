401bacb50062e3dd897d5e139b58b63d6498922f
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-03 08:36:11 +0530

    Time: 34 ms (15.14%), Space: 15.7 MB (92.06%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
-    char nextGreatestLetter(vector<char>& letters, char target) {
-        if(target == 'z')
-            return letters[0];
-        for(auto c : letters) {
-            if(c > target)
-                return c;
+    char nextGreatestLetter(vector<char>& l, char target) {
+       int lo = 0, hi = l.size(), mid;
+        while(lo < hi) {
+            mid = lo + (hi - lo) / 2;
+            if(target >= l[mid])
+                lo = mid + 1;
+            else
+                hi = mid;
         }
-        return letters[0];
+        if(l[lo] == target) lo++;
+        cout<<lo;
+        return l[lo % l.size()];
     }
 };
