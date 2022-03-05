488d73aef0b9d96b3885901ebf591b9ecdec4009
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-05 07:04:47 +0530

    Time: 4 ms (62.06%), Space: 6.7 MB (24.21%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        char res = 'a';
+        char res = 0;
         for(char c : s)
             res ^= c;
         for(char c : t)
             res ^= c;
-        res ^= 'a';
+        //res ^= 'a';
         return res;
     }
 };
