10d84c50fac9917363783fe974c6b2740141c77d
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-06 10:32:57 +0530

    Time: 4 ms (72.81%), Space: 13.5 MB (11.76%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
@@ -12,15 +12,26 @@
 class Solution {
 public:
     int sumOfLeftLeaves(TreeNode* root) {
-        return sum(root, false);
-    }
-    int sum(TreeNode* root, bool left) {
         if(root == NULL)
             return 0;
+        stack<pair<TreeNode*, bool>> st;
+        st.push({root, 0});
+        int res = 0;
+        while(!st.empty()) {
+            root = st.top().first;
+            bool left = st.top().second;
+            st.pop();
+            if(left && root->left == NULL && root->right == NULL) {
+                res += root->val;
+                continue;
+            }
+            if(root->left)
+                st.push({root->left, 1});
+            if(root->right)
+                st.push({root->right, 0});
+        }
         
-        if(left && (root->left == NULL && root->right == NULL))
-            return root->val;
+        return res;
         
-        return sum(root->left, 1) + sum(root->right, 0);
     }
 };
