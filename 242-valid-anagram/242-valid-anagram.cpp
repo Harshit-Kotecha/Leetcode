f064dcf74d5641e124add3fe25986abda33c7678
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-08 10:30:14 +0530

    Time: 4 ms (95.56%), Space: 7.4 MB (47.05%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int cnt[26] = {0};
+        for(char c : s) {
+            cnt[c - 'a']++;
+        }
+        for(char c : t) 
+            cnt[c - 'a']--;
+        for(int i = 0; i < 26; i++)
+            if(cnt[i] != 0)
+                return 0;
+        return 1;
+    }
+};
