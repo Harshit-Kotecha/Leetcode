27fcc7ed9b57712fa6e5a0c18172844ae5aeb961
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-08 07:50:45 +0530

    Time: 119 ms (71.83%), Space: 61.9 MB (28.36%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -19,18 +19,20 @@ public:
         q.push(root);
         while(!q.empty()) {
             int size = q.size();
-            vector<int> level;
+            //vector<int> level;
+            sum = 0;
             for(int i = 0; i < size; i++) {
                 root = q.front();
                 q.pop();
-                level.push_back(root->val);
+                //level.push_back(root->val);
+                sum += root->val;
                 if(root->left) q.push(root->left);
                 if(root->right) q.push(root->right);
             }
-            if(q.empty()) {
-                for(int x : level)
-                    sum += x;
-            }
+            // if(q.empty()) {
+            //     for(int x : level)
+            //         sum += x;
+            // }
         }
         return sum;
     }
