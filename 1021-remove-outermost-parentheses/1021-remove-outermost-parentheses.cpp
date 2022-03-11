0e9f30939d39eb36d854a8e1c7ab69832a9815fe
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-11 10:19:52 +0530

    Time: 7 ms (37.23%), Space: 6.7 MB (54.21%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -4,13 +4,13 @@ public:
         int open = 0, k = 0;
         for(auto c : s) {
             if(c == '(') {
-                open++;
-                if(open > 1)
+                //open++;
+                if(++open > 1)
                     s[k++] = c;
             }
             else {
-                open--;
-                if(open > 0)
+                //open--;
+                if(--open > 0)
                     s[k++] = c;
             }
         }
