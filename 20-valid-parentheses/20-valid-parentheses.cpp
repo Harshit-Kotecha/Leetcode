cdb7af35103d62359cee88d28a53cc34610c0ec1
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-13 07:44:25 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (10.36%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c : s) {
+            if(c == '(') st.push(')');
+            else if(c == '[') st.push(']');
+            else if(c == '{') st.push('}');
+            else if(st.empty() || st.top() != c) return false;
+            else st.pop();
+        }
+        
+        return st.empty();
+    }
+};
