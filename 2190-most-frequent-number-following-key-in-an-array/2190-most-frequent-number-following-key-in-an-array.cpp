b7d6d33879dee60fa34268fa626202424d1f000a
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-14 07:04:20 +0530

    Time: 21 ms (25.40%), Space: 12.4 MB (87.34%) - LeetHub

--- 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.cpp
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int mostFrequent(vector<int>& nums, int key) {
-        int t = nums[1], k = 0, res = 0, cnt = 0;
+        int cnt[1001] = {}, res = 0;
         for(int i = 1; i < nums.size(); i++) {
-            t = nums[i];
-            k = 0;
-            for(int i = 0; i < nums.size() - 1; i++) {
-                if(nums[i] == key && nums[i+1] == t) {
-                    k++;
-                }
+            if(nums[i-1] == key) {
+                cnt[nums[i]]++;
+                if(cnt[nums[i]] > cnt[res])
+                    res = nums[i];
             }
-            if(k > cnt)
-                res = t;
-            
-            cnt = max(cnt, k);
         }
         return res;
     }
