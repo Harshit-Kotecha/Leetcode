17ddf1fe52b4edf7d4618a93ddb3546f37f7d20d
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-15 07:01:53 +0530

    Time: 365 ms (54.72%), Space: 116 MB (65.70%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.cpp
+++ 575-distribute-candies/575-distribute-candies.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     int distributeCandies(vector<int>& nums) {
-        int n = nums.size() / 2;
-        if(n == 0)
-            return 0;
-        sort(nums.begin(), nums.end());
-        int res = 1;
-        for(int i = 1; i < nums.size(); i++) {
-            if(nums[i] != nums[i-1])
-                res++;
+        int n = nums.size() / 2, res = 0;
+        unordered_map<int, bool> map;
+        for(int x : nums)
+            map[x] = 1;
+        for(auto& m : map) {
+            res ++;
             if(res >= n)
                 return min(res, n);
         }
         return res;
+        // if(n == 0)
+        //     return 0;
+        // sort(nums.begin(), nums.end());
+        // int res = 1;
+        // for(int i = 1; i < nums.size(); i++) {
+        //     if(nums[i] != nums[i-1])
+        //         res++;
+        //     if(res >= n)
+        //         return min(res, n);
+        // }
+        // return res;
     }
 };
