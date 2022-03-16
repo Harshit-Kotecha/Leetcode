2505831d7f3c41c10fc773d5ac73ce1175a620fb
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-16 07:26:36 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (81.92%) - LeetHub

--- 290-word-pattern/290-word-pattern.cpp
+++ 290-word-pattern/290-word-pattern.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool wordPattern(string p, string s) {
-        unordered_map<char, int> m1;
         unordered_map<string, int> m2;
-        
+        int cnt[26] = {};
         int i = 0, n = p.length();
         istringstream iss(s);
         while(iss >> s) {
-            if(i == n || m1[p[i]] != m2[s])
+            if(i == n || cnt[p[i] - 'a'] != m2[s])
                 return 0;
             
-            m1[p[i]] = m2[s] = i + 1;
+            cnt[p[i] - 'a'] = m2[s] = i + 1;
             i++;
         }
         
