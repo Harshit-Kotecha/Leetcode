3f673a5cb84b3b2046b22230dfb715cd33c5b8c3
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-18 17:06:09 +0530

    Time: 8 ms (79.34%), Space: 8.4 MB (41.33%) - LeetHub

--- 482-license-key-formatting/482-license-key-formatting.cpp
+++ 482-license-key-formatting/482-license-key-formatting.cpp
@@ -7,25 +7,19 @@ public:
                 s[len++] = c;
             }
         }
-        int rem = len % k;
-        //len /= k;
-        //cout<<rem<<endl;
-        //for(int i = 0; i < len; i++) cout<<s[i]<<" ";
+        int rem = len % k, i = 0;
         string res;
-        for(int i = 0, j = 0; i < len; i++) {
-            if(rem > 0) {
-                while(rem-- > 0) {
-                    res += isalpha(s[i]) ? toupper(s[i]) : s[i];
-                    i++;
-                }
-                i--;
-            } else {
-                j = 0;
-                while(i < len && j < k) {
-                    res += isalpha(s[i]) ? toupper(s[i]) : s[i];
-                    i++, j++;
-                }
-                i--;
+
+        while(rem-- > 0) {
+            res += isalpha(s[i]) ? toupper(s[i]) : s[i];
+            i++;
+        }
+        if(res.size()) res += '-';
+        while(i < len) {
+            int j = 0;
+            while(i < len && j < k) {
+                res += isalpha(s[i]) ? toupper(s[i]) : s[i];
+                i++, j++;
             }
             res += '-';
         }
