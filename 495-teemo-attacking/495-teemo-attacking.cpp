2359e69ae215f36edf24084832b7d42c88b43964
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-18 08:00:39 +0530

    Time: 49 ms (70.22%), Space: 25.8 MB (64.33%) - LeetHub

--- 495-teemo-attacking/495-teemo-attacking.cpp
+++ 495-teemo-attacking/495-teemo-attacking.cpp
@@ -3,13 +3,14 @@ public:
     int findPoisonedDuration(vector<int>& t, int d) {
         int res = d;
         for(int i = 0; i < t.size() - 1; i++) {
-            if(t[i] + d - 1 < t[i+1]) {
-                res += d;
+//             if(t[i] + d - 1 < t[i+1]) {
+//                 res += d;
                 
-            }
-            else {
-                res += (t[i+1] - t[i]);
-            }
+//             }
+//             else {
+//                 res += (t[i+1] - t[i]);
+//             }
+            res += min(d, t[i+1] - t[i]);
         }
         return res;
     }
