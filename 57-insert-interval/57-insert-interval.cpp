0400b1e175210d05c6176787594c3de9327515db
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-21 07:02:22 +0530

    Time: 24 ms (49.22%), Space: 17.2 MB (24.05%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& nums, vector<int>& m) {
+        if(nums.size() == 0) return {m};
+        if(m.size() == 0) return {nums};
         vector<vector<int>> res;
-        nums.push_back(m);
-        sort(nums.begin(), nums.end());
-        for(auto x : nums) {
+        bool done = false;
+        for(auto& x : nums) {
             if(res.empty() || res.back()[1] < x[0]) {
                 res.push_back(x);
             } else {
                 res.back()[1] = max(res.back()[1], x[1]);
             }
+            if(done == false && res.back()[0] > m[0] && res.back()[0] > m[1]) {
+                res.insert(res.end()-1, m);
+                done = true;
+            }
+            else if(done == false && res.back()[1] >= m[0]) {
+                res.back()[0] = min(res.back()[0], m[0]);
+                res.back()[1] = max(res.back()[1], m[1]);
+                done = true;
+            }
         }
-        
+        if(done == false) {
+            if(res.back()[1] < m[0])
+                res.push_back(m);
+            else
+                res.back()[1] = max(res.back()[1], m[1]);
+        }
         return res;
         
     }
