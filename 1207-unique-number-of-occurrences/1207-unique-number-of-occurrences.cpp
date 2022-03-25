ce2ea86bff4eaf465c5703a8b0288ee95f2a814b
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-03-25 10:38:10 +0530

    Time: 8 ms (24.45%), Space: 8 MB (90.77%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& nums) {
-        int cnt[2002] = {0};
-        for(int i = 0; i < nums.size(); i++) {
-            cnt[nums[i] + 1000]++;
+        int cnt[2001] = {};
+        for(int x : nums) {
+            cnt[x + 1000]++;
         }
         sort(begin(cnt), end(cnt));
-        int i = 0;
-        while(cnt[i] == 0) i++;
-        while(i < 2001) {
-            if(cnt[i] == cnt[i+1])
+        for(int i = 0; i < 2000; i++) {
+            if(cnt[i] && cnt[i] == cnt[i+1])
                 return 0;
-            i++;
         }
+        
         return 1;
     }
 };
