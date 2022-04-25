48dac0189859520d34f5ecd4f57a61dc284f130f
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-04-25 08:14:56 +0530

    Time: 11 ms (90.73%), Space: 15.7 MB (6.16%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i = 0; i < nums.size(); i++) {
-            int t = target - nums[i];
-            int lo = i + 1, hi = nums.size() - 1, mid;
-            while(lo < hi) {
-                mid = lo + (hi - lo) / 2;
-                if(t > nums[mid])
-                    lo = mid + 1;
-                else
-                    hi = mid;
-            }
-            if(nums[lo] == t) return {i+1, lo+1};
+        int i = 0, j = nums.size() - 1;
+        while(i < j) {
+            if(nums[i] + nums[j] == target)
+                return {i+1, j+1};
+            else if(nums[i] + nums[j] > target)
+                j--;
+            else
+                i++;
         }
         return {};
     }
