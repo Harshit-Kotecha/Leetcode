b6dcd24fa8ebc2da99253969d4ee7bc004f32f1d
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-04-30 11:44:18 +0530

    Time: 189 ms (72.91%), Space: 96.5 MB (17.70%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.cpp
+++ 896-monotonic-array/896-monotonic-array.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        if(nums.size() <= 2) return 1;
+        // if(nums.size() <= 2) return 1;
         int c1 = 0, c2 = 0;
         for(int i = 1; i < nums.size(); i++) {
             if(nums[i] >= nums[i-1]) c1++;
             if(nums[i] <= nums[i-1]) c2++;
         }
-        cout<<c1<<" "<<c2<<endl;
-        if (c1 == nums.size() - 1 || c2 == nums.size() - 1) return 1;
-        return 0;
+        return (c1 == nums.size() - 1 || c2 == nums.size() - 1);
     }
 };
