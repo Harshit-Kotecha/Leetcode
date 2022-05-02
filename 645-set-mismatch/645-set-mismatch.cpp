1dbf378c63f46fdceb5b5e62a7b40b307cfd80f6
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-05-02 10:51:03 +0530

    Time: 77 ms (29.18%), Space: 30.9 MB (26.88%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.cpp
+++ 645-set-mismatch/645-set-mismatch.cpp
@@ -3,17 +3,16 @@ public:
     vector<int> findErrorNums(vector<int>& nums) {
         unordered_map<int, int> m;
         vector<int> res;
-        for(int i = 1; i <= nums.size(); i++) {
-            m[i]++;
-        }
+        // for(int i = 1; i <= nums.size(); i++) {
+        //     m[i]++;
+        // }
         for(int i : nums) {
-            if(--m[i] < 0) {
+            if(++m[i] > 1) {
                 res.push_back(i);
-                //break;
             }
         }
-        for(auto& i : m) {
-            if(i.second == 1) res.push_back(i.first);
+        for(int i = 1; i <= nums.size(); i++) {
+            if(m[i] == 0) res.push_back(i);
         }
         return res;
     }
