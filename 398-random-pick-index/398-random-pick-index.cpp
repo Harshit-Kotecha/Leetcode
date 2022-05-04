19f5dfc7b316d6e693371ed85131ea23eeb009e6
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-05-04 09:35:21 +0530

    Time: 286 ms (6.70%), Space: 59.8 MB (8.26%) - LeetHub

--- 398-random-pick-index/398-random-pick-index.cpp
+++ 398-random-pick-index/398-random-pick-index.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    vector<int> nums;
     unordered_map<int, vector<int>> m;
     Solution(vector<int>& nums) {
         for(int i = 0; i < nums.size(); i++) {
-            m[nums[i]].push_back(i);
+            this->m[nums[i]].push_back(i);
         }
     }
     
     int pick(int t) {
-        // vector<int> v = m[t];
-        // int r = m[t].size() - 1; cout<<r<<" ";
         int lo = 0, hi = m[t].size() - 1;
         int x = (rand() % (hi - lo + 1)) + lo;
         return m[t][x];
-        // return v[x];
         
     }
 };
