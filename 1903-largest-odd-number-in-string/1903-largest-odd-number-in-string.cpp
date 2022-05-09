7af475ea017bdc375b114657717098b0220bee99
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-05-09 09:42:04 +0530

    Time: 39 ms (33.29%), Space: 14.5 MB (80.17%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -2,20 +2,13 @@ class Solution {
 public:
     string largestOddNumber(string num) {
         if(num.back() & 1) return num;
-        string res;
-        int lo = 0, hi = -1;
+        int hi = -1;
         for(int i = 0; i < num.length(); i++) {
-            // if(lo == -1 && num[i] & 1) {
-            //     //lo = i;
-            //     hi = i;
-            // } else 
             if(num[i] & 1) {
                 hi = i;
             }
         }
         if(hi == -1) return "";
-        // cout<<string(&num[lo], &num[hi+1]);
-        return string(&num[lo], &num[hi+1]);
-        // return res;
+        return string(&num[0], &num[hi+1]);
     }
 };
