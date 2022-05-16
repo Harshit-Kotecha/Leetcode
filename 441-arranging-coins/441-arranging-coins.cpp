a33c5764e59e2937e9e03220b5ecf937ca396214
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-05-16 15:49:52 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (95.33%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int arrangeCoins(int x) {
-        long long n = x;
+    int arrangeCoins(long long n) {
+        //long long n = x;
         return (sqrt(1 + 8*n) - 1) / 2;
     }
 };
