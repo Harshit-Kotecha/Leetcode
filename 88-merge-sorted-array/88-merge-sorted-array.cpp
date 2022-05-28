3333da006734de304ea22641ea56ec8f99dd952e
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-05-28 08:46:34 +0530

    Time: 3 ms (71.90%), Space: 9 MB (90.37%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -9,9 +9,7 @@ public:
                 nums1[k--] = nums2[j--];
             }
         }
-        while(i >= 0) {
-            nums1[k--] = nums1[i--];
-        }
+      
         while(j >= 0) {
             nums1[k--] = nums2[j--];
         }
