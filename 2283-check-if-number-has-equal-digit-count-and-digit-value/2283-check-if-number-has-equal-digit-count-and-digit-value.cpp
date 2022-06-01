30fc589cdd35a9820140aa40071e68bdc3a32ae7
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-06-01 08:02:16 +0530

    Time: 7 ms (17.27%), Space: 5.8 MB (93.88%) - LeetHub

--- 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.cpp
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.cpp
@@ -3,7 +3,10 @@ public:
     bool digitCount(string num) {
         int cnt[11] = {};
         
-        for(char i : num) cnt[i - '0']++;
+        for(int i = 0; i < num.length(); i++) {
+            ++cnt[num[i] - '0'];
+            if(cnt[i] > num[i] - '0') return 0;
+        }
         cout<<cnt['1' - '0'];
         for(int i = 0; i < num.length(); i++) {
             if(cnt[i] != num[i] - '0') return 0;
