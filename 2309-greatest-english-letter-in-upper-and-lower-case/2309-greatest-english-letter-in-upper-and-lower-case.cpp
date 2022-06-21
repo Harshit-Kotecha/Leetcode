ec6637768867192f1d71d9d7d63c40373baf83f2
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-06-21 10:07:35 +0530

    Time: 25 ms (10.58%), Space: 6.8 MB (48.74%) - LeetHub

---
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string greatestLetter(string s) {
+        string res;
+        
+        set<char> st(s.begin(), s.end());
+        
+        for(auto c : s) {
+            if(st.find(tolower(c)) != st.end() && st.find(toupper(c)) != st.end()) {
+                if(res.size()) {
+                    if(toupper(c) > res[0]) res = toupper(c);
+                } else {
+                    res = toupper(c);
+                }
+            }
+        }
+        
+        return res;
+    }
+};
