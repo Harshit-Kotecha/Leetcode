bac8cd025dd41c11534eacd9d6883304d7bede2e
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-06-21 10:13:46 +0530

    Time: 16 ms (20.41%), Space: 6.9 MB (34.17%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     string greatestLetter(string s) {
-        string res;
         
         set<char> st(s.begin(), s.end());
         
-        for(auto c : s) {
-            
-            if(res.size() && res[0] > toupper(c)) continue;
-            
-            if(st.find(tolower(c)) != st.end() && st.find(toupper(c)) != st.end()) {
-                if(res.size()) {
-                    if(toupper(c) > res[0]) res = toupper(c);
-                } else {
-                    res = toupper(c);
-                }
+        for(char ch = 'Z'; ch >= 'A'; ch--) {
+            if(st.find(ch) != st.end() && st.find(tolower(ch)) != st.end()) {
+                return string(1, ch);
             }
         }
         
-        return res;
+        return "";
     }
 };
