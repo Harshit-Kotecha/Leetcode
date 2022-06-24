2a15d3e340c6fa4920b42bf428b2538d4d4bbaa1
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-06-24 08:09:58 +0530

    Time: 1026 ms (23.71%), Space: 107.9 MB (78.07%) - LeetHub

--- 2241-design-an-atm-machine/2241-design-an-atm-machine.cpp
+++ 2241-design-an-atm-machine/2241-design-an-atm-machine.cpp
@@ -1,29 +1,25 @@
 class ATM {
 public:
-    long long bank[5] = {}, val[5] = {20, 50, 100, 200, 500};
+    long long bank[5] = {};
+    long long val[5] = {20, 50, 100, 200, 500};
     ATM() {
         
     }
     
     void deposit(vector<int> banknotesCount) {
-        for(int i = 0; i < banknotesCount.size(); i++) {
+        for(int i = 0; i < 5; i++)
             bank[i] += banknotesCount[i];
-        }
     }
     
     vector<int> withdraw(int amount) {
         vector<int> res(5);
-        
-        for(int i = 4; i >= 0 && amount > 0; i--) {
+        for(int i = 4; i >= 0; i--) {
             res[i] = min(bank[i], amount / val[i]);
             amount -= (res[i] * val[i]);
         }
-        
-        if(amount > 0) return {-1};
-        
-        for(int i = 0; i < 5; i++) {
+        if(amount) return {-1};
+        for(int i = 0; i < 5; i++)
             bank[i] -= res[i];
-        }
         return res;
     }
 };
