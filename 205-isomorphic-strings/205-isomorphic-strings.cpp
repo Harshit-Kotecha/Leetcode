0921e205b85619055d258811a2770db445238a7a
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-06-30 11:25:17 +0530

    Time: 44 ms (5.04%), Space: 8.3 MB (5.07%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -3,20 +3,20 @@ public:
     bool isIsomorphic(string s, string t) {
         if(s.length() != t.length()) return 0;
         
-        unordered_map<char, char> m, p;
+        return transform(s) == transform(t);
+    }
+    string transform(string s) {
+        vector<int> cnt(256, -1);
+        string res; 
         for(int i = 0; i < s.length(); i++) {
-            if(m.find(s[i]) == m.end()) {
-                m[s[i]] = t[i];
-            } else if(m[s[i]] != t[i]) {
-                return 0;
-            }
-            if(p.find(t[i]) == p.end()) {
-                p[t[i]] = s[i];
-            } else if(p[t[i]] != s[i]) {
-                return 0;
+            
+            if(cnt[s[i]] == -1) {
+                res += to_string(i) + ' ';
+                cnt[s[i]] = i;
+            } else {
+                res += to_string(cnt[s[i]]) + ' ';
             }
         }
-        
-        return 1;
+        return res;
     }
 };
