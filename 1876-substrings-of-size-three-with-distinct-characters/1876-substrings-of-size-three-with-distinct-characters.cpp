82252ddeb6755bc8f096ce4eab6aeb1cc533fc6e
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-07-13 10:55:17 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (75.20%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     int countGoodSubstrings(string s) {
         if(s.length() < 3) return 0;
-        //string r = s.substr(0, 3);
         int res = 0;
-        for(int i = 0; i + 3 <= s.length(); i++) {
-            string r = s.substr(i, 3);
-            if(set<char>(r.begin(), r.end()).size() == 3) res++; 
+        for(int i = 1; i + 1 < s.length(); i++) {
+             if(s[i] != s[i-1] && s[i] != s[i+1] && s[i-1] != s[i+1]) res++;
         }
         return res;
     }
