fcd0f0b8bbd8c56943607e21e4e6157b92395203
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-07-15 16:54:27 +0530

    Added solution - LeetHub

--- Level order traversal - GFG/level-order-traversal.cpp
+++ Level order traversal - GFG/level-order-traversal.cpp
@@ -51,13 +51,11 @@ class Solution
       q.push(node);
       while(!q.empty()) {
           
-          for(int i = 0, s = q.size(); i < s; i++) {
               node = q.front();
               q.pop();
               rs.push_back(node->data);
               if(node->left) q.push(node->left);
               if(node->right) q.push(node->right);
-          }
       }
       return rs;
     }
