54db91b9c982d319bf1d79bbdadd8cfa3175e0fb
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-07-15 18:11:14 +0530

    Added solution - LeetHub

---
+++ Print BST elements in given range - GFG/print-bst-elements-in-given-range.cpp
@@ -0,0 +1,133 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+  vector<int> rs;
+    vector<int> printNearNodes(Node *root, int low, int high) {
+        //code here  
+        rs.clear();
+        dfs(root, low, high);
+        sort(rs.begin(), rs.end());
+        return rs;
+    }
+    void dfs(Node* root, int l, int h) {
+        if(root == NULL) return;
+        
+        if(root->data < l) {
+            dfs(root->right, l, h);
+        } else if(root->data > h) {
+            dfs(root->left, l, h);
+        } else if(root->data >= l && root->data <= h) {
+            rs.push_back(root->data);
+            dfs(root->left, l, h);
+            dfs(root->right, l, h);
+        }
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        int k1 , k2;
+        string s;
+		getline(cin,s);
+		scanf("%d ", &k1);
+		scanf("%d " , &k2);
+		Solution ob;
+        Node* root = buildTree(s);
+        vector <int> res = ob.printNearNodes(root, k1 ,  k2);
+        for (int i=0;i<res.size();i++) cout << res[i] << " ";
+        cout<<endl;
+    }
+    return 1;
+}
+  // } Driver Code Ends
