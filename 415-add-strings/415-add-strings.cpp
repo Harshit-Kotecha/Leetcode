5bc4138f9ab71cb6007d882888a77f9ff51c28f5
Harshit-Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-07-25 19:37:26 +0530

    Time: 3 ms (86.99%), Space: 6.7 MB (89.92%) - LeetHub

---
+++ 415-add-strings/415-add-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        if(num1.length() < num2.length()) return addStrings(num2, num1);
+        
+        int i = num1.length() - 1, j = num2.length() - 1, s = 0;
+            
+        while(j >= 0) {
+            s += num1[i] - '0' + num2[j] - '0';
+            num1[i] = (s % 10) + '0';
+            s /= 10;
+            i--, j--;
+        }
+        while(i >= 0 && s) {
+            s += num1[i] - '0';
+            num1[i] = (s % 10) + '0';
+            s /= 10;
+            i--;
+        }
+        if(s) return to_string(s) + num1;
+        return num1;
+    }
+};
