8bb20c4ad47ae96e9f933ec0e133e5133938e9a7
Harshit Kotecha <67956129+Harshit-Kotecha@users.noreply.github.com>
2022-07-28 16:58:57 +0530

    Time: 51 ms (37.71%), Space: 27.3 MB (85.87%) - LeetHub

--- 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.cpp
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> addToArrayForm(vector<int>& num, int k) {
         int i = num.size() - 1, s = 0;
         while(i >= 0) {
-            s += num[i] + (k > 0 ? k%10 : 0);
+            s += num[i] + k%10;
             num[i] = (s % 10);
             s /= 10;
             k /= 10;
@@ -11,12 +11,10 @@ public:
             if(s == 0 && k == 0) return num;
         }
         while(s || k) {
-            if(k) {
-                s += k%10;
-                k /= 10;
-            }
+            s += k%10;
             num.insert(num.begin(), s%10);
             s /= 10;
+            k /= 10;
         }
         return num;
         
